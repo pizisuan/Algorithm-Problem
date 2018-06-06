83553e458802a45ae656c44f7ba62e085cb8646c
Pizisuan <pizisuan@sina.com>
2018-06-06 17:42:29 +0800

    Create Remove_Element.cpp

---
+++ Remove_Element.cpp
@@ -0,0 +1,34 @@
+/*
+ * Problem Description
+ *
+ * Given an array and a value, remove all instances of that value in place and return the new length.
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ *
+ */
+ 
+/*
+ * 题意：在一个数组里面移除指定value，并且返回新的数组长度。
+ * 解决方案：
+ * 使用两个游标i、j遍历数组。如果碰到了value，使用j记录位置，同时递增i，直到下一个非value出现。
+ * 将此时i对应的值复制到j的位置上，增加j，重复上述过程直到遍历结束。这时候j就是新的数组长度。
+ */
+
+class solution
+{
+public:
+	int removeElement(int a[], int n, int elem)
+	{
+		int i = 0;
+		int j = 0;
+		for (i = 0; i < n; i++)
+		{
+			if (a[i] == elem)
+			{
+				continue;
+			}
+			a[j] = a[i];
+			j++;
+		}
+		return j;
+	}
+};
