38a92be295c6c44a0b7ab2ea7f5d0b21d267c8fa
Pizisuan <pizisuan@sina.com>
2018-07-26 11:26:45 +0800

    Update To_Lower_Case.cpp

--- To_Lower_Case.cpp
+++ To_Lower_Case.cpp
@@ -1,33 +1,23 @@
+/*
+ * Problem Description
+ *
+ * Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+ * 
+ * Example 1:
+ * Input: "Hello"  Output: "hello"
+ * 
+ * Example 2:
+ * Input: "here"  Output: "here"
+ */
 
 
-
-
-
-
-
-
-#include "stdafx.h"
-#include <iostream>
-#include <string>
 #include <algorithm>
-
-
-std::string toLowerCase(std::string str)
+class Solution 
 {
-	std::transform(str.begin(), str.end(), str.begin(), ::tolower);
-	return str;
-}
-
-
-int main()
-{
-	std::string test = toLowerCase("HELLO");
-	std::string test2 = toLowerCase("here");
-	std::string test3 = toLowerCase("LOVELY");
-
-	std::cout << test << std::endl;
-	std::cout << test2 << std::endl;
-	std::cout << test3 << std::endl;
-
-  return 0;
-}
+public:
+    string toLowerCase(string str) 
+    {
+        transform(str.begin(),str.end(),str.begin(),::tolower);
+        return str;
+    }
+};
