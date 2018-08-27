d7631a94ba80b11ded41cbec5a5f3bb714268677
Pizisuan <pizisuan@sina.com>
2018-08-27 11:21:59 +0800

    Create Linked_List_Cycle.cpp

---
+++ Linked_List_Cycle.cpp
@@ -0,0 +1,39 @@
+/*
+ * Problem Description
+ *
+ * Given a linked list, determine if it has a cycle in it.
+ *
+ */
+ 
+//思路：设置快慢两个指针，慢指针步长为1，快指针步长为2。如果链表无环，那么快指针比慢指针先到链尾；如果链表有环，那么快指针将会在某一时刻与慢指针重合。 
+
+struct ListNode 
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution 
+{
+public:
+    bool hasCycle(ListNode *head)
+    {
+        if (head == nullptr)
+        {
+            return false;
+        }
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while (slow->next != nullptr && fast->next != nullptr && fast->next->next != nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+};
