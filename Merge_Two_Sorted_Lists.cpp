f7e8d2c0ddc6449b878a8504045177bb6e3c9ee3
Pizisuan <pizisuan@sina.com>
2018-09-06 09:55:47 +0800

    Update Merge_Two_Sorted_Lists.cpp

--- Merge_Two_Sorted_Lists.cpp
+++ Merge_Two_Sorted_Lists.cpp
@@ -42,3 +42,20 @@ public:
         return dummy.next;
     }
 };
+
+// 递归方法实现（拿来主义！！！）
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        
+        if(l1->val < l2->val) {
+            l1->next = mergeTwoLists(l1->next, l2);
+            return l1;
+        } else {
+            l2->next = mergeTwoLists(l2->next, l1);
+            return l2;
+        }
+    }
+};
