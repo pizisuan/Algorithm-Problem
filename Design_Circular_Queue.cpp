6bfe001b062a5e974cd2841fc4d213a72f153538
Pizisuan <pizisuan@sina.com>
2018-09-14 09:07:17 +0800

    Update Design_Circular_Queue.cpp

--- Design_Circular_Queue.cpp
+++ Design_Circular_Queue.cpp
@@ -37,6 +37,7 @@
 
 class MyCircularQueue 
 {
+pirvate:
     vector<int> q;
     int head, tail, cur_len, MAX_LEN;      // head指向队头元素，tail指向队尾元素的后一个位置，cur_len为当前队列中元素的个数，MAX_LEN为队列最大容量
 public:
@@ -112,5 +113,76 @@ public:
     bool isFull() 
     {
         return cur_len == MAX_LEN;
+    }
+};
+
+
+// 官方给出的解答：
+class MyCircularQueue {
+private:
+    vector<int> data;
+    int head;
+    int tail;
+    int size;
+public:
+    /** Initialize your data structure here. Set the size of the queue to be k. */
+    MyCircularQueue(int k) {
+        data.resize(k);
+        head = -1;
+        tail = -1;
+        size = k;
+    }
+    
+    /** Insert an element into the circular queue. Return true if the operation is successful. */
+    bool enQueue(int value) {
+        if (isFull()) {
+            return false;
+        }
+        if (isEmpty()) {
+            head = 0;
+        }
+        tail = (tail + 1) % size;
+        data[tail] = value;
+        return true;
+    }
+    
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
+    bool deQueue() {
+        if (isEmpty()) {
+            return false;
+        }
+        if (head == tail) {
+            head = -1;
+            tail = -1;
+            return true;
+        }
+        head = (head + 1) % size;
+        return true;
+    }
+    
+    /** Get the front item from the queue. */
+    int Front() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return data[head];
+    }
+    
+    /** Get the last item from the queue. */
+    int Rear() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return data[tail];
+    }
+    
+    /** Checks whether the circular queue is empty or not. */
+    bool isEmpty() {
+        return head == -1;
+    }
+    
+    /** Checks whether the circular queue is full or not. */
+    bool isFull() {
+        return ((tail + 1) % size) == head;
     }
 };
