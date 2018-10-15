c043153918d7b1074231cce3626cbdb97a102bfa
Pizisuan <pizisuan@sina.com>
2018-10-15 09:53:49 +0800

    Create Daily_Temperatures.cpp

---
+++ Daily_Temperatures.cpp
@@ -0,0 +1,44 @@
+/*
+ * Problem Description
+ * Given a list of daily temperatures T, return a list such that, for each day in the input, tells you how many days you would have to
+ * wait until a warmer temperature.  If there is no future day for which this is possible, put 0 instead.
+ *
+ * For example, given the list of temperatures T = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+ *
+ */
+
+// 我的代码第一版：遍历（耗时长）
+// 37 test cases passed. Runtime: 1552 ms
+
+
+class Solution 
+{
+public:
+    vector<int> dailyTemperatures(vector<int>& T) 
+	  {
+        if (T.empty())
+        {
+            return {};
+        }
+        vector<int> res;
+        for(size_t i = 0; i < T.size(); ++i)
+        {
+            bool find = false;
+            for(size_t j = i+1; j < T.size(); ++j)
+            {
+                if (T[j] > T[i])
+                {
+                    find = true;
+                    res.push_back(j-i);
+                    break;
+                }
+            }
+            if (!find)
+            {
+                res.push_back(0);
+            }
+        }
+        return res;
+    }
+};
+
